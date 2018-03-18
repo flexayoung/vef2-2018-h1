e2bd231b41595fca42cd98a21ff7419ec0910316
quynh <npquynhdo@gmail.com>
2018-03-18 19:34:44 +0000

    the schema up cast

---
+++ cloudinary/schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE users (
+  id serial primary key,
+  username varchar UNIQUE,
+  password character varying(255) NOT NULL,
+  name varchar NOT NULL,
+  url varchar
+);
+
+CREATE TABLE categories (
+  id serial primary key,
+  name varchar UNIQUE not null
+);
+
+CREATE TABLE books (
+  id serial primary key,
+  title varchar UNIQUE not null,
+  isbn13 text UNIQUE check(isbn13 < 14),
+  author varchar,
+  descr varchar,
+  category varchar REFERENCES categories(name)
+);
+
+CREATE TABLE userbooks(
+  id serial primary key,
+  userID serial REFERENCES users(id),
+  bookID serial REFERENCES books(id),
+  score integer check(score < 6 and score > 0)
+);
